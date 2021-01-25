7064273ec5fe41b59c5107c36c646b463696b8c3
wclotus <64985553+wclotus@users.noreply.github.com>
2021-01-25 09:56:11 +0100

    Add files via upload

---
+++ CameraSwitch.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour {
+
+	public Camera MainCam;
+	public Camera FirstPersonCam;
+	// Use this for initialization
+	void Start () {
+
+		//MainCam = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+		//FirstPersonCam = GameObject.FindWithTag("FirstPersonCamera").GetComponent<Camera>();
+		MainCam.enabled = true;
+		FirstPersonCam.enabled = false;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyUp(KeyCode.C))
+		{
+			MainCam.enabled = !MainCam.enabled;
+			FirstPersonCam.enabled = !FirstPersonCam.enabled;
+			//GameObject.Find("MainCamera").camera.enabled = true;
+			//GameObject.Find("FirstPersonCamera").camera.enabled = false;
+
+		}
+
+	}
+}
---
+++ Move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal")*Time.deltaTime,0f,Input.GetAxis("Vertical")*Time.deltaTime);
+        
+    }
+}
